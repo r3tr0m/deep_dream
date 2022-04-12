8600fd67af6d5ced82e28ead25bd34b1ce71d040
r3tr0m <100650453+r3tr0m@users.noreply.github.com>
2022-04-12 20:16:24 -0300

    Add files via upload

--- dream.c
+++ dream.c
@@ -6,67 +6,40 @@
 #include <sys/types.h>
 #include <time.h>
 
-void bits_stream(uint32_t number){
-
-	uint8_t  remainder;
-	uint32_t qou = number;
-	
-	uint64_t bits[sizeof(number) * 8];
-	uint8_t  inc = 0;
-	
-	while(qou != 0){
-	
-		remainder = qou % 2;
-		qou = qou / 2;
-		
-		bits[inc] = remainder;
-		
-		
-	inc++;
-	}
-	
-	for(uint32_t bit=inc-1; bit < -1; bit--){
-		printf("%hhu",bits[bit]);
-	
-	}
-
-	printf("\n");
-}
-
 char* build_dream(){
-	
+
 	uint64_t epoch_time 	= time(NULL);
-	uint16_t memory_space 	= 500; 
-	
+	uint16_t memory_space 	= 500;
+
 	char *dream = (char*)malloc(memory_space);
-	
+
 	if(dream == NULL){
 		return NULL;
 	}
-	
+
 	char memory_1[memory_space];
 	char memory_2[memory_space];
 	char memory_3[memory_space];
 	char memory_4[memory_space];
-	
+
 	char t_byte;
-	
+
 	for(uint16_t byte=0; byte < memory_space; byte++){
-	
+
 		t_byte = (memory_1[byte] * byte) ^ (memory_2[byte] * byte) ^ (memory_3[byte] * byte) ^ (memory_4[byte] * byte);
-		
+
 		switch(t_byte){
 
 			case 0:
-				t_byte = (byte-1 ^ byte-2 ^ byte-3 ^ byte-4); 
+				t_byte = (byte-1 ^ byte-2 ^ byte-3 ^ byte-4);
 				break;
 			default:
 				break;
 		}
-		
+
 		dream[byte] = t_byte;
 	}
-	
+
 	return dream;
 
 }
@@ -74,10 +47,9 @@ char* build_dream(){
 int64_t random_number(char *table,size_t size_type){
 
 	char* local_dream = build_dream();
-	
+
 	uint64_t epoch_time 	= time(NULL);
 	uint64_t r_number	= ( (uint64_t) local_dream + (uint64_t) epoch_time ) % 500;
-	
 
 	for(uint16_t byte=0; byte < 500; byte++)
 	{
@@ -85,11 +57,9 @@ int64_t random_number(char *table,size_t size_type){
 		r_number += (r_number | table[byte]) | (uint64_t) &local_dream[byte];
 
 	}
-	
 
-	
 	switch(size_type){
-	
+
 		case 1:
 			r_number = r_number % CHAR_MAX;
 			break;
@@ -105,19 +75,7 @@ int64_t random_number(char *table,size_t size_type){
 		default:
 			break;
 	}
-	
 
 	return r_number;
-		
 }
 
-int main(){
-
-	char *table = build_dream();
-	
-	for(uint32_t byte=0; byte < 100000; byte++){
-		random_number(table,2);
-	}
-	
-
-}
